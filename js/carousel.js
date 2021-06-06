e052b79e71ed1e9d4f791947ea33fd1095c1265b
Ubuntu <ubuntu@m152-10-ap18a.maas>
2021-06-06 03:52:41 +0000

    Added sections with animation

---
+++ js/carousel.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.demo').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        // centerMode: true,
+        infinite: true,
+        // cssEase: 'linear',
+        // variableWidth: true, // added
+    });
+  });
+
